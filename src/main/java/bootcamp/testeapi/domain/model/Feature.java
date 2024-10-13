ccd45c29f50c1142ccad3d5e018a4de11d328ddc
Marcos Júnior <marcosajr11@gmail.com>
2024-10-13 19:41:06 -0300

    feat: Feature and News classes

---
+++ src/main/java/bootcamp/testeapi/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package bootcamp.testeapi.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem{
+
+}
---
+++ src/main/java/bootcamp/testeapi/domain/model/News.java
@@ -0,0 +1,7 @@
+package bootcamp.testeapi.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem{
+}
